4c6800c42263733b94c0612dbbfc83318b294de3
ShawnAlisson <shawnalisson@users.noreply.github.com>
2026-06-06 15:06:44 +0100

    Initial Commit

---
+++ HealthLongi/HealthLongiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthLongiApp.swift
+//  HealthLongi
+//
+//  Created by Shayan A Pahlavan on 06/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HealthLongiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
